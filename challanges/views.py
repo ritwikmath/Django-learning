b4bd58e01d2fbf4e1c14b0bb9ca805ace1c72d39
ritwikmath <38456727+ritwikmath@users.noreply.github.com>
2024-01-04 21:40:12 +0530

    Path converter

--- challanges/views.py
+++ challanges/views.py
@@ -5,4 +5,11 @@ import calendar
 def monthly_challange(request, month):
     if not list(calendar.month_name).__contains__(month.capitalize()):
         return HttpResponseNotFound(f"{month} month not supported")
-    return HttpResponse(f"{month}")
+    return HttpResponse(month)
+
+def monthly_challange_by_number(request, month):
+    try:
+        month_name = calendar.month_name[month]
+        return HttpResponse(month_name)
+    except IndexError:
+        return HttpResponseNotFound("Month is not supported")
