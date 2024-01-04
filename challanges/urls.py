b80acc52ca0e80817687336a6d56f804ea389f6b
ritwikmath <38456727+ritwikmath@users.noreply.github.com>
2024-01-04 23:19:45 +0530

    named routes and html response

--- challanges/urls.py
+++ challanges/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path("<int:month>", views.monthly_challange_by_number),
-    path("<str:month>", views.monthly_challange)
+    path("<str:month>", views.monthly_challange, name="monthly-challange")
 ]
--- challanges/views.py
+++ challanges/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 import calendar
+from django.urls import reverse
 
 def monthly_challange(request, month):
     if not list(calendar.month_name).__contains__(month.capitalize()):
-        return HttpResponseNotFound(f"{month} month not supported")
-    return HttpResponseRedirect(f"/challanges/{month}")
+        return HttpResponseNotFound(f"<span style='color:red'>{month} month not supported</span>")
+    search = request.GET.get("search")
+    response_content = f"""
+        <h1>Selected Month is {month}</h1>
+        <h3>Search value is {search or 'Empty'}</h3>
+    """
+    return HttpResponse(response_content)
 
 def monthly_challange_by_number(request, month):
     try:
         month_name = calendar.month_name[month]
-        return HttpResponse(f"Selected month is {month_name}")
+        return HttpResponseRedirect(reverse("monthly-challange", args=[month_name]))
     except IndexError:
         return HttpResponseNotFound("Month is not supported")
