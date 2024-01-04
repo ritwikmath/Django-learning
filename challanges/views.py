aa978e5ceae1e81a3439d87cfad732616bd0a9f7
ritwikmath <38456727+ritwikmath@users.noreply.github.com>
2024-01-04 21:34:37 +0530

    Route parameter

--- challanges/views.py
+++ challanges/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
+import calendar
 
-def index(request):
-    return HttpResponse("Hello Developers!")
+def monthly_challange(request, month):
+    if not list(calendar.month_name).__contains__(month.capitalize()):
+        return HttpResponseNotFound(f"{month} month not supported")
+    return HttpResponse(f"{month}")
