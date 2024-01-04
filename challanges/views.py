6176c37039853dfc182748779c3b62b69c4b0a94
ritwikmath <38456727+ritwikmath@users.noreply.github.com>
2024-01-04 22:17:37 +0530

    http redirect

--- challanges/views.py
+++ challanges/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 import calendar
 
 def monthly_challange(request, month):
     if not list(calendar.month_name).__contains__(month.capitalize()):
         return HttpResponseNotFound(f"{month} month not supported")
-    return HttpResponse(month)
+    return HttpResponseRedirect(f"/challanges/{month}")
 
 def monthly_challange_by_number(request, month):
     try:
         month_name = calendar.month_name[month]
-        return HttpResponse(month_name)
+        return HttpResponse(f"Selected month is {month_name}")
     except IndexError:
         return HttpResponseNotFound("Month is not supported")
