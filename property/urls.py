46074a73b338dc62873fe27775b3a843fa116e80
ritwikmath <38456727+ritwikmath@users.noreply.github.com>
2024-01-06 18:17:14 +0530

    index and contact page with routes
    Active link highlight using request.path

--- property/urls.py
+++ property/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="property-index")
+    path("", views.index, name="property-index"),
+    path("contact", views.contact, name="property-contact")
 ]
